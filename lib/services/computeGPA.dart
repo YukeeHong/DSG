c0ffb90622aaac797eb03f457de0e6e086ad083d
yangyi-zhu <142135488+yangyi-zhu@users.noreply.github.com>
2024-07-11 19:12:02 +0400

    Removed duplicate code

--- lib/services/computeGPA.dart
+++ lib/services/computeGPA.dart
@@ -6,41 +6,11 @@ class computeGPA {
     int totalCredits = 0;
 
     for (var course in courses) {
-      double gradePoint = gradeToPoint(course.grade);
+      double gradePoint = course.gradePoint;
       totalPoints += gradePoint * course.credits;
       totalCredits += course.credits;
     }
 
     return totalCredits == 0 ? 0 : totalPoints / totalCredits;
-  }
-
-  static double gradeToPoint(String grade) {
-    switch (grade) {
-      case 'A+':
-      case 'A':
-        return 5.0;
-      case 'A-':
-        return 4.5;
-      case 'B+':
-        return 4.0;
-      case 'B':
-        return 3.5;
-      case 'B-':
-        return 3.0;
-      case 'C+':
-        return 2.5;
-      case 'C':
-        return 2.0;
-      case 'C-':
-        return 1.5;
-      case 'D+':
-        return 1.0;
-      case 'D':
-        return 0.5;
-      case 'F':
-        return 0.0;
-      default:
-        return 0.0;
-    }
   }
 }

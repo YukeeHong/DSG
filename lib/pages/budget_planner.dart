27aed17b25d37e935145133a4a314c9d9def837c
yangyi-zhu <142135488+yangyi-zhu@users.noreply.github.com>
2024-07-20 02:52:21 +0400

    Added ability to edit records

--- lib/pages/budget_planner.dart
+++ lib/pages/budget_planner.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:nus_orbital_chronos/services/add_bill.dart';
-import 'package:nus_orbital_chronos/services/bill_list.dart';
 import 'package:nus_orbital_chronos/services/bill.dart';
 
 class BudgetPlanner extends StatefulWidget {
@@ -11,14 +9,19 @@ class BudgetPlanner extends StatefulWidget {
 
 class _BudgetPlannerState extends State<BudgetPlanner> {
   late Box<Bill> billsBox;
+  var _bills;
+  final _descriptionController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime? _selectedDate;
 
   @override
   void initState() {
     super.initState();
     billsBox = Hive.box<Bill>('Bills');
+    _bills = billsBox.values.toList();
   }
 
-  void _addBill(String description, double amount, DateTime date, int id) {
+  void addBill(String description, double amount, DateTime date, int id) {
     setState(() {
       billsBox.put(id, Bill(
         description: description,
@@ -29,14 +32,142 @@ class _BudgetPlannerState extends State<BudgetPlanner> {
     });
   }
 
-  void _startAddNewBill(BuildContext context) {
+  void _submitData(int id) {
+    if (_amountController.text.isEmpty) {
+      return;
+    }
+
+    final enteredDescription = _descriptionController.text;
+    final enteredAmount = double.parse(_amountController.text);
+
+    if (enteredDescription.isEmpty ||
+        enteredAmount <= 0 ||
+        _selectedDate == null) {
+      return;
+    }
+
+    if (id == -1) {
+      int firstFree = 0;
+      var bills = billsBox.values.toList();
+      bills.sort((a, b) => a.id.compareTo(b.id));
+      for (Bill bill in bills) {
+        if (bill.id == firstFree) {
+          firstFree++;
+        } else {
+          break;
+        }
+      }
+
+      id = firstFree;
+    }
+
+    addBill(
+      enteredDescription,
+      enteredAmount,
+      _selectedDate!,
+      id,
+    );
+
+    _descriptionController.clear();
+    _amountController.clear();
+    _selectedDate = null;
+
+    _bills = billsBox.values.toList();
+    Navigator.of(context).pop();
+  }
+
+  void _presentDatePicker(DateTime date) {
+    showDatePicker(
+      context: context,
+      initialDate: date,
+      firstDate: DateTime(2020),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
+  }
+
+  void _startAddNewBill(BuildContext context, int id) {
+    if (id != -1) {
+      Bill bill = billsBox.get(id)!;
+      _descriptionController.text = bill.description;
+      _amountController.text = bill.amount.toString();
+      _selectedDate = bill.date;
+    }
+
     showModalBottomSheet(
       context: context,
       builder: (bCtx) {
         return GestureDetector(
           onTap: () {},
           behavior: HitTestBehavior.opaque,
-          child: AddBill(_addBill),
+          child: StatefulBuilder(
+            builder: (BuildContext context, StateSetter setState) {
+              return Card(
+                elevation: 5,
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      TextField(
+                        controller: _descriptionController,
+                        decoration: InputDecoration(labelText: 'Description'),
+                      ),
+                      TextField(
+                        controller: _amountController,
+                        decoration: InputDecoration(labelText: 'Amount'),
+                        keyboardType: TextInputType.number,
+                      ),
+                      Container(
+                        height: 70,
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(
+                                _selectedDate == null
+                                    ? 'No Date Chosen!'
+                                    : 'Picked Date: ${_selectedDate.toString()}',
+                              ),
+                            ),
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                foregroundColor: Theme.of(context).primaryColor,
+                              ),
+                              child: Text(
+                                'Choose Date',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              onPressed: () {
+                                DateTime date = _selectedDate ?? DateTime.now();
+                                _presentDatePicker(date);
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      ElevatedButton(
+                        child: Text('Save'),
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor:
+                          Theme.of(context).primaryColor, // Button text color
+                        ),
+                        onPressed: () {
+                          _submitData(id);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }
+          )
         );
       },
     );
@@ -57,9 +188,7 @@ class _BudgetPlannerState extends State<BudgetPlanner> {
       body: ValueListenableBuilder(
         valueListenable: billsBox.listenable(),
         builder: (context, Box<Bill> billsBox, _) {
-          final _bills = billsBox.values.toList();
-          final totalAmount = _bills.fold(0.0, (double sum, Bill bill) => sum + bill.amount);
-
+          final totalAmount = billsBox.values.fold<double>(0.0, (double sum, Bill bill) => sum + bill.amount);
           return Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
@@ -74,16 +203,39 @@ class _BudgetPlannerState extends State<BudgetPlanner> {
                 ),
               ),
               Expanded(
-                child: BillList(_bills),
+                child: ListView.builder(
+                  itemCount: _bills.length,
+                  itemBuilder: (ctx, index) {
+                    return Card(
+                      child: ListTile(
+                        title: Text(_bills[index].description),
+                        subtitle: Text(
+                          '\$${_bills[index].amount.toStringAsFixed(2)} - ${_bills[index].date.toString()}',
+                        ),
+                        trailing: IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () {
+                            billsBox.delete(_bills[index].id);
+                          },
+                        ),
+                        onTap: () {
+                          _startAddNewBill(context, _bills[index].id);
+                        },
+                      ),
+                    );
+                  },
+                ),
               ),
             ],
           );
         }
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => _startAddNewBill(context),
+        onPressed: () => _startAddNewBill(context, -1),
         child: Icon(Icons.add),
       ),
     );
   }
 }
+
+
